c3cce7837b1935c608064ae8e590d679c6d61c26
joinwithyogeshRK <joinwithyogeshRK@users.noreply.github.com>
2025-08-21 18:15:21 +0530

    Upload src/App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Experience from './pages/Experience';
+import Contact from './pages/Contact';
+import NotFound from './pages/NotFound';
+import Layout from './components/Layout';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="experience" element={<Experience />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
